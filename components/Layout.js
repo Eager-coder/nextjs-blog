2ec8a57226c901df92a6137cb6b73482e0fc9c85
Turan Sultan <turan.sultann@gmail.com>
2020-08-04 00:06:46 +0600

    style

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Header from './Header'
 import Footer from './Footer'
 import '../styles/css/App.css'
+import Head from 'next/head'
 const Layout = ({ children }) => {
 	return (
 		<>
+			<Head>
+				<link rel='shortcut icon' href='/images/icon.png' type='image/x-icon' />
+			</Head>
 			<Header />
 			<main>{children}</main>
 			<Footer />
